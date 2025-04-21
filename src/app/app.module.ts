1cf17db68c4d3409f10562ce9b29618678272f13
Takkino31 <takkinoyaya@gmail.com>
2025-04-21 12:22:44 +0000

    Nav Menu DONE

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,25 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { AdminTemplateComponent } from './admin-template/admin-template.component';
+import { MatToolbarModule} from '@angular/material/toolbar';
+import { MatButtonModule} from '@angular/material/button';
+import { MatIconModule} from '@angular/material/icon';
+import { MatMenuModule, MatMenuTrigger} from '@angular/material/menu';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AdminTemplateComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    MatToolbarModule,
+    MatButtonModule,
+    MatIconModule,
+    MatMenuTrigger,
+    MatMenuModule
   ],
   providers: [],
   bootstrap: [AppComponent]
