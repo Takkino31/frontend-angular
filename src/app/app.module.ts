a9d0403b12a9b5a300cbbbc8b0688c4a97af5a26
Takkino31 <takkinoyaya@gmail.com>
2025-04-21 22:45:28 +0000

    Students fetching without back-end && Filter Search DONE

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -9,7 +9,7 @@ import { MatButtonModule} from '@angular/material/button';
 import { MatIconModule} from '@angular/material/icon';
 import { MatMenuModule, MatMenuTrigger} from '@angular/material/menu';
 import { MatSidenavModule} from '@angular/material/sidenav';
-import {MatListItem, MatListModule} from '@angular/material/list';
+import { MatListItem, MatListModule} from '@angular/material/list';
 import { LoginComponent } from './login/login.component';
 import { PaymentsComponent } from './payments/payments.component';
 import { ProfileComponent } from './profile/profile.component';
@@ -19,6 +19,12 @@ import { HomeComponent } from './home/home.component';
 import {MatCardModule} from '@angular/material/card';
 import { LoadStudentsComponent } from './load-students/load-students.component';
 import { LoadPaymentsComponent } from './load-payments/load-payments.component';
+import {MatTableModule} from '@angular/material/table';
+import {MatDividerModule} from '@angular/material/divider';
+import {MatPaginatorModule} from '@angular/material/paginator';
+import { MatSortModule} from '@angular/material/sort';
+import {MatInput, MatInputModule} from '@angular/material/input';
+import {MatFormFieldModule} from '@angular/material/form-field';
 
 @NgModule({
   declarations: [
@@ -45,6 +51,13 @@ import { LoadPaymentsComponent } from './load-payments/load-payments.component';
     MatListItem,
     MatListModule,
     MatCardModule,
+    MatTableModule,
+    MatDividerModule,
+    MatPaginatorModule,
+    MatSortModule,
+    MatInput,
+    MatFormFieldModule
+
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/students/students.component.ts
+++ src/app/students/students.component.ts
@@ -1,4 +1,8 @@
-import { Component } from '@angular/core';
+import {AfterViewInit, Component, OnInit, ViewChild} from '@angular/core';
+import {MatTableDataSource} from '@angular/material/table';
+import {MatPaginator} from '@angular/material/paginator';
+import { MatSort} from '@angular/material/sort';
+import {Router} from '@angular/router';
 
 @Component({
   selector: 'app-students',
@@ -6,6 +10,45 @@ import { Component } from '@angular/core';
   templateUrl: './students.component.html',
   styleUrl: './students.component.css'
 })
-export class StudentsComponent {
+export class StudentsComponent implements OnInit, AfterViewInit{
 
+  public students : any;
+  public dataSource : any;
+  public displayedColumns = ['id', 'firstName','lastName','payments'];
+  @ViewChild(MatPaginator) paginator !: MatPaginator;
+  @ViewChild(MatSort) sort !: MatSort;
+
+  constructor(
+    private router : Router
+  ) {
+  }
+
+  ngOnInit() {
+    this.students = [];
+    for (let i = 0; i < 100; i++) {
+      this.students.push(
+        {
+          id : i+1,
+          firstName : Math.random().toString(20),
+          lastName : `Student+${i}`,
+        }
+      )
+    }
+    this.dataSource = new MatTableDataSource(this.students)
+  }
+
+  ngAfterViewInit(): void {
+    this.dataSource.paginator = this.paginator
+    this.dataSource.sort = this.sort
+  }
+
+
+  filterStudents(event: Event) {
+    let value = (event.target as HTMLInputElement).value
+    this.dataSource.filter = value
+  }
+
+  getPayments(student: any) {
+    this.router.navigateByUrl("/payments")
+  }
 }
